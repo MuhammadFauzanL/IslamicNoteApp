425394b7d196ed0992782d1b95653206a0dcab0b
M Fauzan Lubada <fauzanlubada5@gmail.com>
2025-12-30 21:19:43 +0700

    Update profile page, fix authentication state, and add chatbot services

--- lib/services/chat_api.dart
+++ lib/services/chat_api.dart
@@ -3,23 +3,23 @@ import 'package:http/http.dart' as http;
 
 class ChatApi {
   static const String baseUrl =
-      "http://127.0.0.1:5005/chat"; // GANTI INI
+      "https://web-production-40a77.up.railway.app/chat";
 
   static Future<Map<String, dynamic>> sendMessage(
-      String text, String sessionId) async {
-    final response = await http.post(
+    String query,
+    String sessionId,
+  ) async {
+    final res = await http.post(
       Uri.parse(baseUrl),
-      headers: {"Content-Type": "application/json"},
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: jsonEncode({
-        "query": text,
+        "query": query,
         "session_id": sessionId,
       }),
     );
 
-    if (response.statusCode != 200) {
-      throw Exception("Server error");
-    }
-
-    return jsonDecode(response.body);
+    return jsonDecode(res.body);
   }
 }
